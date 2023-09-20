bb298519a32498e0466e7be99dddb30dc27a2733
torealdemir <aldemirtore@gmail.com>
2023-09-20 20:38:05 +0300

    some product added items section and admin models handle

--- markett/markett/urls.py
+++ markett/markett/urls.py
@@ -1,9 +1,11 @@
 from core.views import index, contact
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', index, name='index'),
     path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
