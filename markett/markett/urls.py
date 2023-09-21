655e941666d699ee04b520e1f40e7417b91fbe06
torealdemir <aldemirtore@gmail.com>
2023-09-21 10:47:00 +0300

    some more changes

--- markett/markett/urls.py
+++ markett/markett/urls.py
@@ -1,11 +1,11 @@
 from core.views import index, contact
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('contact/', contact, name='contact'),
+    path('', index, include('core.urls')),
+    path('items/', include('item.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
